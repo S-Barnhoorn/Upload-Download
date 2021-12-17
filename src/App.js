e5904384291a9ed1ad59210c3e0be8886cbebf6d
s-barnhoorn <sambarnhoorn@gmail.com>
2021-12-17 11:36:47 +0100

    added routing

--- src/App.js
+++ src/App.js
@@ -1,13 +1,35 @@
-
 import './App.css';
-import {NavLink} from "react-router-dom";
-
+import React from 'react';
+import {Route, NavLink, Switch} from 'react-router-dom';
 import Homepage from "./Pages/Homepage/Homepage";
+import Fileupload from "./Pages/Fileupload/Fileupload";
+import Singleupload from "./Pages/Singleupload/Singleupload";
+import Multipleupload from "./Pages/Multipleupload/Multipleupload";
+
 
 function App() {
   return (
     <div>
-      <Homepage/>
+        <NavLink to='/'/>
+        <NavLink to='fileupload'/>
+        <NavLink to='singleupload'/>
+        <NavLink to='multipleupload'/>
+
+        <Switch>
+            <Route exact path='/'>
+                <Homepage/>
+            </Route>
+            <Route exact path='/fileupload'>
+                <Fileupload/>
+            </Route>
+            <Route exact path='/singleupload'>
+                <Singleupload/>
+            </Route>
+            <Route exact path='/multipleupload'>
+                <Multipleupload/>
+            </Route>
+        </Switch>
+
     </div>
   );
 }
